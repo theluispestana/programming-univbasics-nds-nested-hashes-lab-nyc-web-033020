fb6b00d73f387564cb60e67cb7424fa4cd40b721
Luis Pestana <lpestana@protonmail.com>
2020-02-27 22:50:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,14 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  aoh = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]
+  aoh
 end
 
 def aoh_lookup(aoh, row, key)
